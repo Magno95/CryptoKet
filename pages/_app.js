13f878284f43970f521e65a07cd69c122cc573a5
Davide <magnoni.davide@outlook.it>
2023-01-21 15:01:08 +0100

    set app.js theme provider import script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 /* eslint-disable import/no-unresolved */
 import '@/styles/globals.css';
+import { Footer, Navbar } from '@/components';
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/94806df23e.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 export default App;
