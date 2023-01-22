fb33f7040198b37672f0f4e88541423452df7e94
Davide <magnoni.davide@outlook.it>
2023-01-22 15:38:47 +0100

    top sellers cards

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Banner from './Banner';
+import CreatorCard from './CreatorCard';
 
-export { Navbar, Footer };
+export { Navbar, Footer, Banner, CreatorCard };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,93 @@
+import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+import { Banner, CreatorCard } from '../components';
 
-const Home = () => (
-  <div>
-    <h1 className="text-3xl font-bold underline">cia</h1>
-  </div>
-);
+import images from '../assets';
+import { makeId } from '../utils';
+
+const Home = () => {
+  const [hideButtons, setHideButtons] = useState(false);
+  const { theme } = useTheme();
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
+
+  const handleScroll = (dir) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (dir === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollWidth >= parent?.offsetWidth) {
+      setHideButtons(false);
+    } else {
+      setHideButtons(true);
+    }
+  };
+
+  useEffect(() => {
+    isScrollable();
+    window.addEventListener('resize', isScrollable);
+    return () => {
+      window.removeEventListener('resize', isScrollable);
+    };
+  });
+
+  const mockUsrs = ['0x232...kmmd', '0x345...klsp', '0x233...abid', '0x325...ldjn', '0x999...23md', '0x987...a8jd', '0x233...abid'];
+
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+          name=" Discover, collect and sell extraordinary NFTs"
+        />
+        <div>
+          <h1 className="font-poppin dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 sx:ml-0">Best Creators</h1>
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+            <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+              {[6, 7, 8, 9, 10].map((i, index) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i}`]}
+                  creatorName={`${mockUsrs[index]}`}
+                  creatorEths={10 - i * 0.5}
+                />
+              ))}
+              {!hideButtons && (
+                <>
+                  <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                    <div className="relative w-full h-full">
+                      <Image src={images.left} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+                    </div>
+                  </div>
+                  <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                    <div className="relative w-full h-full">
+                      <Image src={images.right} layout="fill" objectFit="contain" alt="right_arrow" className={`w-8 h-8 minlg:w-12 minlg:h-12 ${theme === 'light' ? 'filter invert' : undefined}`} />
+                    </div>
+                  </div>
+
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
 
 export default Home;
