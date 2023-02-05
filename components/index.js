b754accd1e192bc1fbe6508ef430ae618a23e26a
Davide <magnoni.davide@outlook.it>
2023-02-05 13:53:11 +0100

    create nft page

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer';
 import Banner from './Banner';
 import CreatorCard from './CreatorCard';
 import NFTCard from './NFTCard';
+import Input from './Input';
 
-export { Navbar, Footer, Banner, CreatorCard, NFTCard };
+export { Navbar, Footer, Banner, CreatorCard, NFTCard, Input };
